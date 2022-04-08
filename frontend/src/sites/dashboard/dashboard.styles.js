a1e87d03a028b19b95fb2558b76d752c5bf4cb0d
Mateusz Skarupski <skaiipii@gmail.com>
2022-04-08 23:01:46 +0200

    minor code refactor

---
+++ frontend/src/sites/dashboard/dashboard.styles.js
@@ -0,0 +1,3 @@
+import { Box } from "@mui/material";
+
+export const SidebarWrapper = ({children}) => <Box sx={{ minWidth: '200px', paddingTop: '10px', borderRight: 'solid 1px black' }}>{children}</Box>;
