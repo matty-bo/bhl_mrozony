43eaec0cb06d7aada7b487ec2f8eed1384105cb3
matty-bo <mateuszborowiecki.ik@gmail.com>
2022-04-09 08:14:00 +0200

    allow cors all origins

--- backend/main/app.py
+++ backend/main/app.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import water_meters
 
-
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(water_meters.router)
 
 
