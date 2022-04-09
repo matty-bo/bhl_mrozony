6e0f219caacee02b3d2339499d6656168e3cab9e
Mateusz Skarupski <skaiipii@gmail.com>
2022-04-09 09:48:15 +0200

    Add real data fetch for dashboard charts

--- frontend/src/components/barChart/barChart.js
+++ frontend/src/components/barChart/barChart.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { Chart as ChartJS, registerables } from 'chart.js';
 import { Chart } from "react-chartjs-2";
-import { toHour } from '../../utils/hours';
 import { chartStyle } from './barChart.styles';
 ChartJS.register(...registerables);
 
 const dataToColors = (data) => {
-  return data.map((d) => d > 0.8 ? 'rgba(255, 99, 132, 0.2)' : 'rgba(54, 162, 235, 0.2)')
+  return data.map((d) => d.usage > 0.8 ? 'rgba(255, 99, 132, 0.2)' : 'rgba(54, 162, 235, 0.2)')
 };
 
-export const DashboardBarChart = () => {
-  const data = [...new Array(24)].map((el, i) => Math.abs(Math.cos(i/5)) * Math.random());
+export const DashboardBarChart = (props) => {
+  const data = props.data.usages;
+  console.log(data);
 
   return(
     <Chart
       style={chartStyle}
       type='bar'
       data={{
-        labels: [...new Array(24)].map((el, i) => `${ toHour(i) }`),
+        labels: data.map((el) => `${ el.time.slice(0,5) }`),
         datasets: [{
           label: 'Godzinowe zużycie wody',
-          data,
+          data: data.map((el) => el.usage),
           backgroundColor: dataToColors(data)
         }],
       }}
