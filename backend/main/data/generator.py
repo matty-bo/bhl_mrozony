6112c35dacbc4b9975a8013f6bf0be9cf37e36f0
matty-bo <mateuszborowiecki.ik@gmail.com>
2022-04-09 12:07:49 +0200

    added alerts endpoint

--- backend/main/data/generator.py
+++ backend/main/data/generator.py
@@ -1,4 +1,5 @@
 from ..models.water_meters import Measurement, Watermeter
+from ..models.notifications import Alert
 from datetime import time, date, timedelta
 import random
 
@@ -40,3 +41,23 @@ def generate_day_measurements(watermeter: Watermeter, day: str):
         day_list.append(m)
         MEASUREMENT_ID_SEQ += 1
     return day_list
+
+
+def generate_alerts(userID):
+    a1 = Alert(**{
+        'id': 0,
+        'userID': userID,
+        'title': 'Przerwy w dostawie wody',
+        'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem mauris, viverra at diam a, suscipit commodo urna. Etiam rhoncus ornare rutrum. Etiam in nisl posuere, condimentum orci id, semper nisi. Phasellus posuere sem id est ornare, laoreet finibus diam dignissim. Mauris vulputate blandit est, nec auctor metus venenatis vitae. Morbi maximus bibendum diam, ac lacinia est viverra vel. Pellentesque quis vulputate libero. Sed ultricies, urna id tincidunt mattis, dolor sem auctor leo, eu faucibus urna ex at erat. Mauris sed urna sem.',
+        'date': '2022-02-05',
+    })
+
+    a2 = Alert(**{
+        'id': 1,
+        'userID': userID,
+        'title': 'Alert niskiego poziomu wody',
+        'body': 'Ut elit neque, sodales nec risus quis, convallis gravida tellus. Curabitur sit amet diam non odio finibus molestie. Pellentesque in mi eget sapien efficitur vehicula. Quisque a pulvinar elit, at ornare metus. Donec tincidunt congue tellus aliquam pharetra. Pellentesque molestie lacus a ipsum posuere, at bibendum nisl viverra. In sed suscipit lacus. Nunc vel neque aliquam, condimentum velit a, congue enim. Vivamus est elit, accumsan eget suscipit id, egestas nec massa.',
+        'date': '2022-04-23',
+    })
+
+    return [a1, a2]
